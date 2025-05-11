b28227d7a10c492ee51cf8e53459897187c17ea7
Rudra130 <rt1766285@gmail.com>
2025-05-11 17:43:10 +0530

    title and logout changes

--- Front-end/src/components/Layout.tsx
+++ Front-end/src/components/Layout.tsx
@@ -87,7 +87,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
                 size="sm" 
                 onClick={() => {
                   localStorage.removeItem('isAuthenticated');
-                  window.location.href = '/auth';
+                  window.location.href = 'https://arogya-vritti-front-end.onrender.com/';
                 }}
                 className="text-gray-700"
               >
